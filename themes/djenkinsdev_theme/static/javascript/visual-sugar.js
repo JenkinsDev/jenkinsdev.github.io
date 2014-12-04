eedb97954b580eaacd762974110497f0ddd7a988
David Jenkins <david.nicholas.jenkins@gmail.com>
2014-12-04 16:19:52 -0500

    Added gulp-uglifyjs and added the MIT license.

---
+++ themes/djenkinsdev_theme/static/javascript/visual-sugar.js
@@ -0,0 +1,8 @@
+/**
+ * Poor naming scheme of the file, but this file will just hold
+ * basic visual 'sugar' for the front-end of the template.  Nothing
+ * too data intensive, basic fading in of text, etc.
+ *
+ * @author David Jenkins
+ */
+console.log('atesasdfasdfasdft');
