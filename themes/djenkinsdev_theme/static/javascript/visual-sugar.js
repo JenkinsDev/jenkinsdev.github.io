b7d369a8505f4d610db28e9aa75fdda1c045d3e8
David Jenkins <david.nicholas.jenkins@gmail.com>
2014-12-04 16:54:35 -0500

    Added some nice sub-header syntactic sugar

--- themes/djenkinsdev_theme/static/javascript/visual-sugar.js
+++ themes/djenkinsdev_theme/static/javascript/visual-sugar.js
@@ -5,4 +5,20 @@
  *
  * @author David Jenkins
  */
-console.log('atesasdfasdfasdftasdf');
+(function(window, document, undefined) {
+    function load_random_subheader() {
+        var sub_header_ele = document.getElementById('sub-header'),
+            header_text = '',
+            sub_header_text_array = [
+                'The Mind Of David.',
+                'Cool Code Is Cool.'
+            ];
+
+        // We want to be able to display a random sub header text for each page load.  To do that we will
+        // go ahead and do a tiny bit of JS randomness and choose the text based on its index.
+        header_text = sub_header_text_array[Math.floor(Math.random() * sub_header_text_array.length)];
+        sub_header_ele.innerHTML = header_text;
+    }
+
+    window.onload = load_random_subheader;
+})(window, document, undefined);
