366d66ecb2a766d5af375c51540a0122dd15f0ac
David Jenkins <david.nicholas.jenkins@gmail.com>
2019-04-27 10:15:03 -0400

    Initial commit of the source for jenkinsdev.github.io. User & Org pages both require that you use the master branch for deployment, instead of the gh-pages branch that you use for project pages. Moved the source files to this `source` branch.

---
+++ themes/djenkinsdev_theme/tools/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    minifyCSS = require('gulp-minify-css'),
+    less = require('gulp-less'),
+    uglify = require('gulp-uglifyjs');
+
+/**
+ * Constants For Our Paths
+ */
+LESS_FILES = "../static/less/**/*.less";
+MAIN_LESS_FILE = "../static/less/main.less";
+CSS_PATH = "../static/css/";
+
+UNMINIFIED_JS_FILES = ["../static/javascript/**/*.js", "!../static/javascript/minified/script.js"];
+MINIFIED_JS_FILE = "script.js";
+MINIFIED_JS_PATH = "../static/javascript/minified/";
+
+
+
+/***************************************************
+ *                  TASKS
+ **************************************************/
+
+gulp.task('css', function() {
+  gulp.src(MAIN_LESS_FILE)
+      .pipe(less())
+      .pipe(minifyCSS())
+      .pipe(gulp.dest(CSS_PATH));
+});
+
+gulp.task('js', function() {
+  gulp.src(UNMINIFIED_JS_FILES)
+      .pipe(uglify(MINIFIED_JS_FILE))
+      .pipe(gulp.dest(MINIFIED_JS_PATH))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(LESS_FILES, ['css']);
+  gulp.watch(UNMINIFIED_JS_FILES, ['js']);
+});
+
+gulp.task('default', ['watch']);
