98e35c046009ded90866dd238b004bdf5a2bf7c7
David Jenkins <djenkinsdev@gmail.com>
2024-01-09 12:42:02 -0500

    Update CI to now use `obsidian_to_hugo` extension script

---
+++ scripts/obsidian_to_hugo_extension.py
@@ -0,0 +1,60 @@
+from os import stat
+from obsidian_to_hugo import ObsidianToHugo
+
+def add_appropriate_yaml_header(file_contents):
+    return """
+---
+title: {}
+date: {}
+draft: false
+---
+{}
+""".format(file_contents['title'], file_contents['date'], file_contents)
+
+class ObsidianToHugoOverride(ObsidianToHugo):
+
+    def process_content(self) -> None:
+        """
+        Looping through all files in the hugo content directory and replace the
+        wiki links of each matching file.
+        """
+        for root, dirs, files in os.walk(self.hugo_content_dir):
+            for file in files:
+                if not file.endswith(".md"):
+                    continue
+                title = file[:-3]
+                timestamp = stat(os.path.join(root, file)).st_mtime
+                date = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d")
+                content = None
+                with open(os.path.join(root, file), "r", encoding="utf-8") as f:
+                    content = f.read()
+                # If the file matches any of the filters, delete it.
+                keep_file = True
+                for filter in self.filters:
+                    if not filter(content, file):
+                        os.remove(os.path.join(root, file))
+                        keep_file = False
+                        break
+                if not keep_file:
+                    continue
+                for processor in self.processors:
+                    content = processor(content)
+                final_content = """
+---
+title: {}
+date: {}
+draft: false
+---
+{}
+""".format(title, date, content).strip()
+                with open(os.path.join(root, file), "w", encoding="utf-8") as f:
+                    f.write(final_content)
+
+
+
+
+def main():
+    ObsidianToHugo().run()
+
+if __name__ == '__main__':
+    main()
